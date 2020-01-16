2056da0d5c4e87645758e150502b8804ba4fab9a
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-01-17 00:46:15 +0900

    [update] 別のインスタンス変数を使いまわした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @user_summary = UserSummary.last
     @user_summaries = UserSummary.all
     @trends = Trend.where(result_date: Date.today).sample(10)
   end
