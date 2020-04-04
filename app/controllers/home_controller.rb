8a1b09052beb3f1cc7b54ce9ca8e10757a49e67a
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-04-04 21:04:30 +0900

    [add] 人気ツイートの表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@
 class HomeController < ApplicationController
   def index
     @user_summaries = UserSummary.all
+    @popular_tweets = Tweet.order(favorite_count: :desc).limit(3)
     @trends = Trend.where(result_date: Date.today).sample(10)
   end
 end
