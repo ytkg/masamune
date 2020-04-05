a448253b1877d32850457a55533f3e549db4217e
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-04-05 21:03:34 +0900

    [update] トレンド関連削除

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,5 @@ class HomeController < ApplicationController
     @user_summaries = UserSummary.all
     @popular_tweets_by_favorite_count = Tweet.order(favorite_count: :desc).limit(3)
     @popular_tweets_by_retweet_count = Tweet.order(retweet_count: :desc).limit(3)
-    @trends = Trend.where(result_date: Date.today).sample(10)
   end
 end
