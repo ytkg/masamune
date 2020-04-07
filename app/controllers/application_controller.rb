02219b7c31116730ed5466a674c81bf865a99308
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-04-08 01:25:11 +0900

    [update] ユーザーのアイコン、名前をヘッダーに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_user
+
+  private
+
+  def set_user
+    @user = UserSummary.last
+  end
 end
