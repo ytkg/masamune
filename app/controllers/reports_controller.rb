fc64344afcfdce56bdc670351330d836c279f04c
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-04-11 01:14:47 +0900

    [clean] rubocop

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
   def index
-    @tweet_by_year_month = Tweet.select("date_trunc('month', tweeted_at) AS year_month, count(id) as total_tweet_count, sum(retweet_count) as total_retweet_count, sum(favorite_count) as total_favorite_count").group("date_trunc('month', tweeted_at)").order(year_month: :desc)
+    @tweet_by_year_month = Tweet.select("date_trunc('month', tweeted_at) as year_month")
+                                .select('count(id) as total_tweet_count')
+                                .select('sum(retweet_count) as total_retweet_count')
+                                .select('sum(favorite_count) as total_favorite_count')
+                                .group("date_trunc('month', tweeted_at)")
+                                .order(year_month: :desc)
   end
 end
