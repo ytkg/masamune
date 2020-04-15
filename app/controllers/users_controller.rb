29025ca4447c7abf500aacb0dc8db81d0d9093c7
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-04-16 00:35:47 +0900

    [update] 相互フォローのユーザー一覧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def index
     @friends = User.is_friend.decorate
     @followers = User.is_follower.decorate
+    @friend_and_follower_users = @friends & @followers
   end
 end
