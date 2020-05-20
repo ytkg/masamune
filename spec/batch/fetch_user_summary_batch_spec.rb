57efc88005803aeeeedd1d785a182ad36cca0336
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-05-20 22:12:50 +0900

    [add] FetchUserSummaryBatchのテスト追加

---
+++ spec/batch/fetch_user_summary_batch_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe FetchUserSummaryBatch do
+  describe '.call' do
+    before do
+      user = Twitter::User.new(JSON.parse(File.read('spec/json/user.json'), symbolize_names: true))
+      fetch_user_summary_batch = described_class.new
+      allow(fetch_user_summary_batch).to receive(:fetch_user).and_return(user)
+      fetch_user_summary_batch.execute
+    end
+
+    it do
+      expect(UserSummary.first).to have_attributes(name: 'よっぴ',
+                                                   screen_name: 'ytkg_dev',
+                                                   profile_image_url_https: 'https://pbs.twimg.com/profile_images/1217102670705524736/OwV2JC5B_normal.jpg',
+                                                   friends_count: 1603,
+                                                   followers_count: 1746,
+                                                   statuses_count: 228)
+    end
+  end
+end
