4bc043c5affd5a37b1bd20aaf8ecfe4805850b54
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-08-24 23:13:20 +0900

    [add] ユーザーをフォローするバッチ追加

---
+++ app/batch/follow_users_batch.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class FollowUsersBatch
+  def execute
+    users = fetch_users_to_follow
+    follow_users(users)
+
+    Slack::NotificationService.call("#{users.count}人をフォローしました")
+  end
+
+  private
+
+  def fetch_users_from_search(keyword)
+    Twitter::FetchUsersByKeywordSearchService.call(keyword)
+  end
+
+  def fetch_users_to_follow
+    followed_user_ids = User.is_followed.ids
+    count = 0
+    limit_count = rand(5..12)
+    users_from_search = fetch_users_from_search('#プロスピA')
+    users_from_search.each_with_object([]) do |user, arr|
+      next if followed_user_ids.include?(user.id)
+      next if user.friends_count < 50
+      next if user.followers_count < 50
+      next if user.followers_count.to_f / user.friends_count > 1.6
+      next if user.description.exclude?('プロスピ')
+      next if count >= limit_count
+
+      arr.push(user)
+      count += 1
+    end
+  end
+
+  def follow_users(users)
+    users.each do |user|
+      Twitter::FollowUserService.call(user)
+      sleep rand(1.0..8.0)
+    end
+  end
+end
