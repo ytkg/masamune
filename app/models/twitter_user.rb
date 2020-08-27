e18cd3c085dd48ed8522c5297e7cc34bb42bfdc3
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-08-28 02:15:13 +0900

    [add] create_or_updateメソッド追加

--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -1,2 +1,15 @@
 class TwitterUser < ApplicationRecord
+  def self.update_or_create(user)
+    find_or_initialize_by(id: user.id).tap do |twitter_user|
+      twitter_user.update!(
+        screen_name: user.screen_name,
+        name: user.name,
+        statuses_count: user.statuses_count,
+        friends_count: user.friends_count,
+        followers_count: user.followers_count,
+        listed_count: user.listed_count,
+        favourites_count: user.favourites_count
+      )
+    end
+  end
 end
