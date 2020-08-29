761ada2206800dba7b2fbed83b8184ad82050f49
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-08-30 01:29:53 +0900

    [fix] access_token, access_token_secretの指定変更

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -23,8 +23,8 @@ class AdminUser < ApplicationRecord
     Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_API_CONSUMER_KEY']
       config.consumer_secret     = ENV['TWITTER_API_CONSUMER_SECRET']
-      config.access_token        = ENV['TWITTER_API_ACCESS_TOKEN']
-      config.access_token_secret = ENV['TWITTER_API_ACCESS_TOKEN_SECRET']
+      config.access_token        = token
+      config.access_token_secret = secret
     end
   end
 end
