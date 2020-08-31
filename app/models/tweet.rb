6bf90e4f3d4429811bd0f178656cfa43fa4efeec
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-08-31 22:57:39 +0900

    [add] update_or_createメソッド追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,15 @@
 
 class Tweet < ApplicationRecord
   belongs_to :admin_user
+
+  def self.update_or_create(tweet)
+    find_or_initialize_by(id: tweet.id).tap do |tweet_|
+      tweet_.update!(
+        tweeted_at: tweet.created_at,
+        text: tweet.text,
+        retweet_count: tweet.retweet_count,
+        favorite_count: tweet.favorite_count
+      )
+    end
+  end
 end
---
+++ spec/model/tweet_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Tweet, type: :model do
+  describe '.update_or_create' do
+    let(:admin_user) { create(:admin_user) }
+    let(:tweet) { Twitter::Tweet.new(JSON.parse(File.read('spec/json/tweet.json'), symbolize_names: true)) }
+
+    subject { admin_user.tweets.update_or_create(tweet) }
+
+    it do
+      is_expected.to have_attributes(id: tweet.id,
+                                     tweeted_at: tweet.created_at,
+                                     text: tweet.text,
+                                     retweet_count: tweet.retweet_count,
+                                     favorite_count: tweet.favorite_count)
+    end
+  end
+end
