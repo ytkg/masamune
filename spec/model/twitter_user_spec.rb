aa2b941b95cd3683a5cd58496d6e24dff891ac54
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-08-31 22:59:58 +0900

    [update] subject指定

--- spec/model/twitter_user_spec.rb
+++ spec/model/twitter_user_spec.rb
@@ -4,16 +4,17 @@ RSpec.describe TwitterUser, type: :model do
   describe '.update_or_create' do
     let(:user) { Twitter::User.new(JSON.parse(File.read('spec/json/twitter_user.json'), symbolize_names: true)) }
 
+    subject { described_class.update_or_create(user) }
+
     it do
-      twitter_user = described_class.update_or_create(user)
-      expect(user).to have_attributes(id: user.id,
-                                      screen_name: user.screen_name,
-                                      name: user.name,
-                                      statuses_count: user.statuses_count,
-                                      friends_count: user.friends_count,
-                                      followers_count: user.followers_count,
-                                      listed_count: user.listed_count,
-                                      favourites_count: user.favourites_count)
+      is_expected.to have_attributes(id: user.id,
+                                     screen_name: user.screen_name,
+                                     name: user.name,
+                                     statuses_count: user.statuses_count,
+                                     friends_count: user.friends_count,
+                                     followers_count: user.followers_count,
+                                     listed_count: user.listed_count,
+                                     favourites_count: user.favourites_count)
     end
   end
 end
