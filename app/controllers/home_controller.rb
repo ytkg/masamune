92452922b682d159378651d97211cad8e4b74288
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-09-05 20:49:29 +0900

    [update] 変数名変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 
 class HomeController < ApplicationController
   def index
-    @user_summaries = current_user.summaries
+    @summaries = current_user.summaries
     @popular_tweets_by_favorite_count = current_user.tweets.order(favorite_count: :desc).limit(3)
     @popular_tweets_by_retweet_count = current_user.tweets.order(retweet_count: :desc).limit(3)
   end
