ccb825e3a4b9f4d1b4a18f58c5fd370a429676c8
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-09-05 20:26:53 +0900

    [update] フォローされていないユーザーとファンを表示するタブ追加

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,6 +2,8 @@ class FriendshipsController < ApplicationController
   def index
     @friend_users = current_user.friend_users.decorate
     @follower_users = current_user.follower_users.decorate
-    @friend_and_follower_users =  @friend_users & @follower_users
+    @friend_and_follower_users = @friend_users & @follower_users
+    @friend_except_follower_users = @friend_users - @follower_users
+    @follower_except_friend_users = @follower_users - @friend_users
   end
 end
