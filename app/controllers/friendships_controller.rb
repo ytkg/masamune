1f85d21f4250b588aeff3168486284b31835c6cc
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-09-10 20:38:44 +0900

    [update] 新しく追加されたユーザー順に並び替え

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,7 @@
 class FriendshipsController < ApplicationController
   def index
-    @friend_users = current_user.friend_users.decorate
-    @follower_users = current_user.follower_users.decorate
+    @friend_users = current_user.friend_users.order(created_at: :desc).decorate
+    @follower_users = current_user.follower_users.order(created_at: :desc).decorate
     @friend_and_follower_users = @friend_users & @follower_users
     @friend_except_follower_users = @friend_users - @follower_users
     @follower_except_friend_users = @follower_users - @friend_users
