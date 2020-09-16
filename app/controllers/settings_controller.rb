91d07da4c25195a823d7b539a7a909b6f236e5f2
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-09-16 23:15:28 +0900

    [update] 自動フォロー設定

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -5,17 +5,22 @@ class SettingsController < ApplicationController
     @settings = current_user.detail.settings || {}
     @settings[:auto_follow] ||= {}
     @settings[:auto_follow][:search_keyword] ||= '#プロスピ'
-    @settings[:auto_follow][:friends_count] ||= 50
-    @settings[:auto_follow][:followers_count] ||= 50
+    @settings[:auto_follow][:friends_count_ge] ||= 50
+    @settings[:auto_follow][:friends_count_le] ||= 10000
+    @settings[:auto_follow][:followers_count_ge] ||= 50
+    @settings[:auto_follow][:followers_count_le] ||= 10000
   end
 
   def create
     current_user.detail.update_settings(
       {
         auto_follow: {
+          enabled: params[:auto_follow_enabled].presence,
           search_keyword: params[:search_keyword].presence,
-          friends_count: params[:friends_count].to_i,
-          followers_count: params[:followers_count].to_i
+          friends_count_ge: params[:friends_count_ge].to_i,
+          friends_count_le: params[:friends_count_le].to_i,
+          followers_count_ge: params[:followers_count_ge].to_i,
+          followers_count_le: params[:followers_count_le].to_i
         }
       }
     )
