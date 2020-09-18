6e08bd320083daaa8d43d907f271bc62eb294b04
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-09-18 23:08:01 +0900

    [fix] 最終更新日が正しくない状態だったのでorder入れた

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 
 class HomeController < ApplicationController
   def index
-    @summaries = current_user.summaries.decorate
+    @summaries = current_user.summaries.order(:id).decorate
     @popular_tweets_by_favorite_count = current_user.tweets.order(favorite_count: :desc).limit(3)
     @popular_tweets_by_retweet_count = current_user.tweets.order(retweet_count: :desc).limit(3)
   end
