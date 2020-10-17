294d0d9c43c04d5646f2fd2b28586420eb3950b4
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-10-17 23:03:08 +0900

    [update] FF比率でもフィルタリングするように

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,27 +2,25 @@
 
 class SettingsController < ApplicationController
   def index
-    @settings = current_user.detail.settings || {}
-    @settings[:auto_follow] ||= {}
-    @settings[:auto_follow][:search_keyword] ||= '#プロスピ'
-    @settings[:auto_follow][:friends_count_ge] ||= 50
-    @settings[:auto_follow][:friends_count_le] ||= 10000
-    @settings[:auto_follow][:followers_count_ge] ||= 50
-    @settings[:auto_follow][:followers_count_le] ||= 10000
+    @settings = current_user.detail.settings || { auto_follow: {} }
   end
 
   def create
-    current_user.detail.update_settings(
-      {
-        auto_follow: {
-          enabled: params[:auto_follow_enabled].presence,
-          search_keyword: params[:search_keyword].presence,
-          friends_count_ge: params[:friends_count_ge].to_i,
-          friends_count_le: params[:friends_count_le].to_i,
-          followers_count_ge: params[:followers_count_ge].to_i,
-          followers_count_le: params[:followers_count_le].to_i
-        }
-      }
-    )
+    current_user.detail.update_settings({ auto_follow: auto_follow_params })
+  end
+
+  private
+
+  def auto_follow_params
+    {
+      enabled: params[:auto_follow_enabled].presence,
+      search_keyword: params[:search_keyword].presence,
+      friends_count_ge: params[:friends_count_ge].to_i,
+      friends_count_le: params[:friends_count_le].to_i,
+      followers_count_ge: params[:followers_count_ge].to_i,
+      followers_count_le: params[:followers_count_le].to_i,
+      ff_ratio_ge: params[:ff_ratio_ge].to_f,
+      ff_ratio_le: params[:ff_ratio_le].to_f
+    }
   end
 end
