6ec6d629476fb2ca82bb512ce2129f92549a24f7
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-06-23 00:38:16 +0900

    [update] support chartkick 4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("chartkick")
-require("chart.js")
+require("chartkick/chart.js")
 require("admin-lte")
 
 
